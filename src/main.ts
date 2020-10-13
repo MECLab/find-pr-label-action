5c7dc01c973b407eb7174d80e1abfbe67f1e9d64
keagan.soakell-ho <keagan.soakell-ho@xero.com>
2020-10-13 14:13:55 +1100

    feat: added debug.yml workflow that runs the action for debugging

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import * as core from '@actions/core'
+import {context} from '@actions/github'
 
 async function run(): Promise<void> {
     try {
-        const labels: string = core.getInput('labels')
-        core.debug(`Finding ${labels}`)
+        const labels: string = core.getInput('anyOf')
+        const pr_labels = context.payload.pull_request?.labels || []
+
+        core.debug(`find using: ${JSON.stringify({labels, pr_labels}, null, 2)}`)
 
         // core.setOutput('time', new Date().toTimeString())
     } catch (error) {
